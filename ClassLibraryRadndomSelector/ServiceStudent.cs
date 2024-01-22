48f968a6df41d2a3905c6a0c54fab5bef7ad9a8d
Zakaria Guerbaoui <salsabile-mp@hotmail.com>
2024-01-22 11:57:31 +0100

    begin

---
+++ ClassLibraryRadndomSelector/ServiceStudent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryRadndomSelector
+{
+    public class ServiceStudent
+    {
+        public List<Student> Students = new List<Student>();
+
+        public ServiceStudent()
+        {
+            
+        }
+
+        public void AddStudent()
+        {
+            Students.Add(new Student());
+        }
+
+        public void RemoveStudent (Student student)
+        {
+            Students.Remove(student);
+        }
+    }
+}
